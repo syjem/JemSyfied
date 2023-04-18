df131f3ec0a216626f16500428b831bc54402c75
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-04-18 16:07:53 +0800

    form validation to its finest

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
-import os
-import datetime
+import re
 
-from flask import Flask, render_template, flash, request, session, url_for
-from werkzeug.security import check_password_hash, generate_password_hash 
+from flask import Flask, render_template, flash, request, session, url_for, redirect
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
 
 app = Flask(__name__)
-
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 # Set Flask app environment to development
 app.debug = 'development'
 
 # Enable debug mode
 app.debug = True
 
+current_date = datetime.now().date()
+
 @app.context_processor
 def inject_current_year():
-    current_year = datetime.datetime.now().year
+    current_year = datetime.now().year
     return dict(current_year=current_year)
+
 
 @app.route("/")
 def home():
@@ -29,9 +32,58 @@ def about():
 def gallery():
     return render_template("gallery.html")
 
-@app.route("/contact")
+SERVICES = [
+    "Weddings",
+    "Portraits",
+    "Families",
+    "Graduations",
+    "Birthdays",
+    "Other Occasions"
+]
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        date = request.form.get("date")
+        service = request.form.get("select_services")
+
+        # Initialize an empty list to store validation errors
+        errors = []
+
+        # form validation
+        if not name:
+             errors.append("Please enter a valid name!")
+        elif len(name) < 3:
+            errors.append("Name is too short!")
+        if not email or not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            errors.append("Please enter a valid email!")
+        if not date:
+           errors.append("Please select a valid date!")
+        elif datetime.strptime(date, '%Y-%m-%d').date() < current_date:
+            errors.append("Please select dates that are not prior to the current date.")
+        if service not in SERVICES:
+            errors.append("Please select a valid service!")
+
+        if errors:
+            # If there are any errors, flash them all at once as error messages
+            for error in errors:
+                flash(error, category='error')
+
+            # Render the form template with error messages and retained form data
+            return render_template("contact.html", services=SERVICES, 
+                                    name=name, email=email, date=date,
+                                    selected_service=service
+                                )
+
+        else:
+            flash("You have booked successfully!", category='success')
+    
+    return render_template("contact.html", services=SERVICES)
+
+
+
 
 
 if __name__ == '__main__':
